3356a3fffc64bbf9a145be3c6ba8baa2af7f6cbc
Haoming Xia <mrmist3r311@gmail.com>
2022-03-07 15:05:55 -0800

    Sets Assignment

--- Sets Assignment/src/SetsAssignment/Main.java
+++ Sets Assignment/src/SetsAssignment/Main.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Locale;
 
 public class Main {
-    public static String rstrip(String line) {
+    public static String rstrip(String _line) {
+        String line = _line;
+
         line.toLowerCase(Locale.ROOT);
-        line = line.replaceAll("[^a-z0-9 ]", ""); // catch all nonalphanumeric
+        line = line.replaceAll("[^a-z0-9 ]", ""); // catch all space/nonalphanumeric characters
         return line;
     }
 
@@ -18,7 +20,7 @@ public class Main {
         try {
             BufferedReader br = new BufferedReader(new FileReader(illiad.getAbsolutePath()));
             HashSet<String> uniqueWords = new HashSet<>();
-            String line = null;
+            String line = "";
 
             while ((line = br.readLine()) != null) {
                 line = rstrip(line);
@@ -26,7 +28,6 @@ public class Main {
             }
 
             System.out.println(uniqueWords.size());
-            System.out.println(uniqueWords);
 
         } catch (IOException e) {
             System.err.println("File Not found!");
