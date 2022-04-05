b4a9bf029aa916e72ab65c84d29043f1fd1f1021
Haoming Xia <mrmist3r311@gmail.com>
2022-04-05 14:58:21 -0700

    bfs assignment

---
+++ Module3.3Graphs/src/com/zaremba/graphs/Main.java
@@ -0,0 +1,70 @@
+package com.zaremba.graphs;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static ArrayList<Node> graph;
+    public static void main(String[] args) throws FileNotFoundException {
+        setupTree();
+        BFS();
+    }
+
+    private static void BFS() {
+        ArrayList<Node> queue = new ArrayList<>();
+
+        for (int childNode : graph.get(0).getNodes()) {
+            queue.add(graph.get(childNode));
+        }
+
+        BFSHelper(queue);
+    }
+
+    private static void BFSHelper(ArrayList<Node> queue) {
+        if (queue.size() == 0)
+            return;
+
+        ArrayList<Node> nextIterationQueue = new ArrayList<>();
+
+        for (Node node : queue) {
+            System.out.println("Visiting " + node.toString());
+
+            for (int c = 0; c <= node.getNodes().size(); c++) {
+                Node childNode = graph.get(c);
+
+                if (!childNode.isVisited()) {
+                    nextIterationQueue.add(childNode);
+                }
+
+
+            }
+
+            graph.get(node.getKey() - 1).setVisited(true);
+        }
+
+        BFSHelper(nextIterationQueue);
+    }
+
+    private static void setupTree() throws FileNotFoundException {
+        graph = new ArrayList<>();
+        Scanner scan = new Scanner(new File("tree.txt"));
+        while(scan.hasNext()){
+            String line = scan.nextLine();
+            parseLine(line);
+        }
+    }
+
+    private static void parseLine(String line) {
+        String[] keys = line.split(" ");
+        int key = Integer.parseInt(keys[0]);
+        ArrayList<Integer> points = new ArrayList<>();
+        for(int i = 1; i < keys.length;i++){
+            points.add(Integer.parseInt(keys[i]));
+        }
+        graph.add(new Node(key, points));
+    }
+}
---
+++ Module3.3Graphs/src/com/zaremba/graphs/Node.java
@@ -0,0 +1,57 @@
+package com.zaremba.graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private int key;
+    private List<Integer> nodes;
+    private boolean visited;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public List<Integer> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(List<Integer> nodes) {
+        this.nodes = nodes;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public Node(int key, ArrayList<Integer> nodes){
+        this.key = key;
+        this.nodes = nodes;
+    }
+
+    @Override
+    public int hashCode() {
+        return key;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Node other){
+            return this.key == other.key;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Node: " + key;
+    }
+}
